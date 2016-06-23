c2b100ed72823b64d9471c6a6d3255f7966c8c73
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-06-23 08:33:13 +0100

    max intervention and stability probabilities in settings. Labels for them in settings UI

--- cocos2d/assets/scripts/Settings.js
+++ cocos2d/assets/scripts/Settings.js
@@ -1,4 +1,6 @@
 
 module.exports = {
-    stochasticity: 20
+    stochasticity: 20,
+    maxInterventionProbability: 1,
+    maxStabilityProbability: 1
 }
--- cocos2d/assets/scripts/SettingsControls.js
+++ cocos2d/assets/scripts/SettingsControls.js
@@ -1,7 +1,23 @@
+var settings = require('Settings');
+
 cc.Class({
     extends: cc.Component,
     
     back: function() {
         cc.director.loadScene("Game");
+    },
+    
+    increaseMaxInterventionProbability: function() {
+        settings.maxInterventionProbability = Math.max(settings.maxInterventionProbability + 0.1, 1);
+        this._updateDisplay();
+    },
+    
+    decreaseMaxInterventionProbability: function() {
+        settings.maxInterventionProbability = Math.max(settings.maxInterventionProbability - 0.1, 0);
+        this._updateDisplay();
+    },
+    
+    _updateDisplay: function () {
+        // this.uncertaintyDisplay.string = settings.stochasticity;
     }
 });
