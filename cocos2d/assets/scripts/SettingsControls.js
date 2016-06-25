178a1337661aef001e1b294551ccded9c7c8880c
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-06-25 11:27:08 +0100

    Settings page updates max probability and stability

--- cocos2d/assets/scripts/SettingsControls.js
+++ cocos2d/assets/scripts/SettingsControls.js
@@ -3,12 +3,27 @@ var settings = require('Settings');
 cc.Class({
     extends: cc.Component,
     
+    properties: {
+        maxInterventionDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        maxStabilityDisplay: {
+            default: null,
+            type: cc.Label
+        }
+    },
+    
+    onLoad: function() {
+      this._updateDisplay();  
+    },
+    
     back: function() {
         cc.director.loadScene("Game");
     },
     
     increaseMaxInterventionProbability: function() {
-        settings.maxInterventionProbability = Math.max(settings.maxInterventionProbability + 0.1, 1);
+        settings.maxInterventionProbability = Math.min(settings.maxInterventionProbability + 0.1, 1);
         this._updateDisplay();
     },
     
@@ -17,7 +32,18 @@ cc.Class({
         this._updateDisplay();
     },
     
+    increaseMaxStabilityProbability: function() {
+        settings.maxStabilityProbability = Math.min(settings.maxStabilityProbability + 0.1, 1);
+        this._updateDisplay();
+    },
+    
+    decreaseMaxStabilityProbability: function() {
+        settings.maxStabilityProbability = Math.max(settings.maxStabilityProbability - 0.1, 0);
+        this._updateDisplay();
+    },
+    
     _updateDisplay: function () {
-        // this.uncertaintyDisplay.string = settings.stochasticity;
+        this.maxInterventionDisplay.string = settings.maxInterventionProbability.toFixed(1);
+        this.maxStabilityDisplay.string = settings.maxStabilityProbability.toFixed(1);
     }
 });
